f93a2840acf99a1e87dd2442cc5aec28a8a16612
SM-Shameem <rony.shameem@gmail.com>
2022-09-22 20:45:23 +0600

    personal library (ver-2) update-3

---
+++ script/reference.js
@@ -0,0 +1,60 @@
+function ShowReferences(arg) {    
+    if (arg != 1) document.getElementById('course1').style.display = 'none';
+    else document.getElementById('reference1').style.display = 'block';
+    if (arg != 2) document.getElementById('course2').style.display = 'none';
+    if (arg != 3) document.getElementById('course3').style.display = 'none';
+    if (arg != 4) document.getElementById('course4').style.display = 'none';
+    if (arg != 5) document.getElementById('course5').style.display = 'none';
+    if (arg != 6) document.getElementById('course6').style.display = 'none';
+    if (arg != 7) document.getElementById('course7').style.display = 'none';
+
+    buttonCourse();
+}
+
+function ShowCourses() {
+    document.getElementById('course1').style.display = 'block';
+    document.getElementById('course2').style.display = 'block';
+    document.getElementById('course3').style.display = 'block';
+    document.getElementById('course4').style.display = 'block';
+    document.getElementById('course5').style.display = 'block';
+    document.getElementById('course6').style.display = 'block';
+    document.getElementById('course7').style.display = 'block';
+
+    document.getElementById('reference1').style.display = 'none';
+
+    buttonReference()
+}
+
+function buttonCourse() {
+    document.getElementById('buttReff_1').style.display = 'none';
+    document.getElementById('buttCour_1').style.display = 'block';
+    document.getElementById('buttReff_2').style.display = 'none';
+    document.getElementById('buttCour_2').style.display = 'block';
+    document.getElementById('buttReff_3').style.display = 'none';
+    document.getElementById('buttCour_3').style.display = 'block';
+    document.getElementById('buttReff_4').style.display = 'none';
+    document.getElementById('buttCour_4').style.display = 'block';
+    document.getElementById('buttReff_5').style.display = 'none';
+    document.getElementById('buttCour_5').style.display = 'block';
+    document.getElementById('buttReff_6').style.display = 'none';
+    document.getElementById('buttCour_6').style.display = 'block';
+    document.getElementById('buttReff_7').style.display = 'none';
+    document.getElementById('buttCour_7').style.display = 'block';
+}
+
+function buttonReference() {
+    document.getElementById('buttReff_1').style.display = 'block';
+    document.getElementById('buttCour_1').style.display = 'none';
+    document.getElementById('buttReff_2').style.display = 'block';
+    document.getElementById('buttCour_2').style.display = 'none';
+    document.getElementById('buttReff_3').style.display = 'block';
+    document.getElementById('buttCour_3').style.display = 'none';
+    document.getElementById('buttReff_4').style.display = 'block';
+    document.getElementById('buttCour_4').style.display = 'none';
+    document.getElementById('buttReff_5').style.display = 'block';
+    document.getElementById('buttCour_5').style.display = 'none';
+    document.getElementById('buttReff_6').style.display = 'block';
+    document.getElementById('buttCour_6').style.display = 'none';
+    document.getElementById('buttReff_7').style.display = 'block';
+    document.getElementById('buttCour_7').style.display = 'none';
+}
